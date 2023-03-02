63cc8694ac8c8d57f8ebb38faaeeef9a5f2006d2
MirikAkhmedov <akhmedovmirik@gmail.com>
2023-03-02 23:38:30 +0500

    wip

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import tokenize from "@/plugins/tokenize";
 
 const instance = axios.create({
-    baseURL: process.env.BACKEND_URL,
+    baseURL: process.env.VUE_APP_BACKEND_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
